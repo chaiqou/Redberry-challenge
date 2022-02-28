990acd5ea23087aafe045120146260b0dc6abd0b
chaiqou <lomtadzenikusha@gmail.com>
2022-02-28 18:40:55 +0400

    Clean unnecessary CRA files, created folder structure

---
+++ src/pages/radbarry-page/Radbarry.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Radbarry = () => {
+  return <div>Radbarry</div>;
+};
+
+export default Radbarry;
---
+++ src/pages/surveylist-page/SurveyList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SurveyList = () => {
+  return <div>SurveyList</div>;
+};
+
+export default SurveyList;
