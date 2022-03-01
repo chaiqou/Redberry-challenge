0fa4c12251c4ac982cce488af46edf7472361433
chaiqou <lomtadzenikusha@gmail.com>
2022-03-01 13:43:19 +0400

    fixed header styles for large screens

--- src/pages/personal-page/Personal.jsx
+++ src/pages/personal-page/Personal.jsx
@@ -9,15 +9,17 @@ const Personal = () => {
   }, []);
 
   // reusable text styles
-  const textStyles = `absolute w-[45vw] h-[20vh] font-['Rowdies'] text-redBerry
-  non-italic font-normal text-6xl leading-tight`;
+  const textStyles = `absolute w-[45vw] h-[30vh] font-['Rowdies'] text-redBerry
+  non-italic font-normal text-5xl leading-tight`;
 
   return (
     <SplitContainer>
       <h1 className={`left-[5vw] top-[7vh] ${textStyles}`}>
         Hey, Rocketeer, what are your coordinates?
       </h1>
-      <h1 className={`left-[60vw] top-[7vh] flex items-center  ${textStyles}`}>
+      <h1
+        className={`left-[60vw] top-[7vh] flex items-center  ${textStyles} xl: w-[60vw] h-[60vh] leading-none`}
+      >
         Redberry Origin
       </h1>
       <p
