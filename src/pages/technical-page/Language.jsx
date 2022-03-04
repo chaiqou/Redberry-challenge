55af17f72e2d2bd68166849fd671811bb9f4e874
chaiqou <lomtadzenikusha@gmail.com>
2022-03-05 01:18:34 +0400

    for language containers rewrite css styles fixed some bug with tailwind and replace with pure css for 2 component

--- src/pages/technical-page/Language.jsx
+++ src/pages/technical-page/Language.jsx
@@ -1,10 +1,9 @@
+import "./languages.css";
+
 const Language = ({ skills, experience }) => {
   return (
-    <div>
-      <li className="first-letter:text-7xl first-letter:font-bold  flex text-sm justify-between font-['Montserrat'] italic font-normal leading-5 items-center ">
-        {` ${skills}  Years of Experience ${experience}`}
-      </li>
-      {/* <button type="button">bt</button> */}
+    <div className="language">
+      <li className="language-item">{`${skills}  Years of Experience ${experience}`}</li>
     </div>
   );
 };
--- src/pages/technical-page/LanguagesList.jsx
+++ src/pages/technical-page/LanguagesList.jsx
@@ -1,12 +1,12 @@
 import Language from "./Language";
 import { reusableStylesField } from "../../helpers/ReusableStyles";
 import { v4 as uuidv4 } from "uuid";
+import "./languages.css";
 
 const LanguagesList = ({ userValues }) => {
-  console.log(userValues);
   return (
-    <div className={`${reusableStylesField} top-[620px]`}>
-      <ul>
+    <div className="flex justify-center items-center flex-col h-[100px] w-34 absolute left-[250px] top-[650px]">
+      <ul className="min-w-[10%] list-none">
         {userValues.map((userValue) => (
           <Language
             key={uuidv4()}
