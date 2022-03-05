08de5b68b2c6d97f7676fa559863a15d9486f36f
chaiqou <lomtadzenikusha@gmail.com>
2022-03-05 16:33:22 +0400

    Filtred array with duplicate skills , now possible add 1 skill only one time

--- src/pages/technical-page/Language.jsx
+++ src/pages/technical-page/Language.jsx
@@ -4,12 +4,15 @@ const Language = ({
   skills,
   experience,
   setUserValues,
-  userValues,
+  UniqueFilteredArray,
   userValue,
 }) => {
   const deleteHandler = () => {
-    setUserValues(userValues.filter((el) => el.skills !== userValue.skills));
+    setUserValues(
+      UniqueFilteredArray.filter((el) => el.skills !== userValue.skills)
+    );
   };
+
   return (
     <div className="m-2 bg-white text-black flex justify-between items-center transition-all">
       <li className="flex items-center justify-between font-medium h-[100%] w-screen   p-3 mb-3 text-base text-white  bg-redBerry rounded-lg dark:bg-red-600 dark:text-red-800">
--- src/pages/technical-page/LanguageList.jsx
+++ src/pages/technical-page/LanguageList.jsx
@@ -2,17 +2,30 @@ import Language from "./Language";
 import { v4 as uuidv4 } from "uuid";
 
 const LanguagesList = ({ userValues, setUserValues }) => {
+  // am constantat meshveobit vfiltravt arrays tu arrayshi ertxel an metjer avirchevt erti da igive skills
+  // am shemtxvevashi duplicated skill ar aisaxeba arrayshi da shesabamisad ar gadaecema Language components
+  // romlis meshveobitac shevdzlebt mxolod unique skillebis damatebas
+
+  const UniqueFilteredArray = Object.values(
+    userValues.reduce((unique, o) => {
+      if (!unique[o.skills] || +o.experience > +unique[o.skills].experience)
+        unique[o.skills] = o;
+
+      return unique;
+    }, {})
+  );
+
   return (
     <div
       className={`flex justify-center items-center flex-col h-[140px] w-68 absolute left-[180px] top-[650px]`}
     >
       <ul className=" w-[490px] list-none ">
-        {userValues.map((userValue, index) => (
+        {UniqueFilteredArray.map((userValue) => (
           <Language
             key={uuidv4()}
             userValue={userValue}
             skills={userValue.skills}
-            userValues={userValues}
+            UniqueFilteredArray={UniqueFilteredArray}
             experience={userValue.experience}
             setUserValues={setUserValues}
           />
