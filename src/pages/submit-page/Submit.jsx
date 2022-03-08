01462925c82b7b48e281e2417cad30813f99e054
chaiqou <lomtadzenikusha@gmail.com>
2022-03-08 13:41:25 +0400

    styled thanks and submit pages

--- src/pages/submit-page/Submit.jsx
+++ src/pages/submit-page/Submit.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Submit = () => {
-  return <div>Submit</div>;
+  return (
+    <div className="h-screen bg-black">
+      <Link
+        to="/thanks"
+        className="animate-bounce-in absolute w-[12rem] left-[754px] top-[522px]  text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl
+         focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-6 mb-6"
+      >
+        Submit
+      </Link>
+      <Link
+        to="/personal"
+        className="animate-bounce-in absolute w-[12rem] left-[754px] top-[582px]  text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl
+        focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-6 mb-6"
+      >
+        Back
+      </Link>
+    </div>
+  );
 };
 
 export default Submit;
--- src/pages/thanks-page/Thanks.jsx
+++ src/pages/thanks-page/Thanks.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Thanks = () => {
-  return <div>Thanks</div>;
+  return (
+    <div className="h-screen bg-black">
+      <h1 className="absolute w-[1066px] text-[#FE3B1F] h-[191px] left-[472px] top-[444px] font-['Rowdies'] text-[96px] leading-[119px] flex items-center text-center">
+        Thanks for Joining 😊
+      </h1>
+    </div>
+  );
 };
 
 export default Thanks;
